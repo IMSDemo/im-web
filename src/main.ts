040c0552efb5343e7e5687249392067016bf0e38
YiBin Huang <13672800746@163.com>
2023-08-04 15:31:06 +0800

    🚨test(custom): 测试功能

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import ArcoVue from '@arco-design/web-vue'
 import App from './App.vue'
+import '@arco-design/web-vue/dist/arco.css'
 import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router).use(ArcoVue)
 
 app.mount('#app')
